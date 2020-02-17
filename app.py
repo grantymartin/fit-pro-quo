f0dd7eeddb27a6d614d289bf9e22e10e1f6ea4bd
Ubuntu <ubuntu@ip-172-31-84-199.ec2.internal>
2020-02-17 15:10:15 +0000

    pymongo added to requirements.txt

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ def delete_action(action_id):
 def sports():
     action=mongo.db.action.find()
     action_trackers=mongo.db.action_tracker.find()
-    #action_tracker_count=action_trackers.count()((action_tracker=action_tracker_count))
-    return render_template("sports.html", actions=action, action_tracker=action_trackers )
+    action_tracker_count=action_trackers.count('action_name')
+    return render_template("sports.html", actions=action, action_tracker=action_tracker_count)
     
 @app.route('/addsport')
 def addsport():
